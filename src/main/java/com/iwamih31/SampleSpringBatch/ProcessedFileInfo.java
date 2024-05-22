005783c073cc57e26bc310d41f620de599cb2283
iwamih31 <iwamih31@gmail.com>
2024-05-22 22:45:20 +0900

    ProcessedFileInfo.java作成

---
+++ src/main/java/com/iwamih31/SampleSpringBatch/ProcessedFileInfo.java
@@ -0,0 +1,24 @@
+package com.iwamih31.SampleSpringBatch;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "file_info")
+@Data
+public class ProcessedFileInfo {
+
+	@Id
+	private String id;
+
+	private String name;
+
+	private int age;
+
+	@Column(name = "is_adult")
+	private boolean isAdult;
+
+}
